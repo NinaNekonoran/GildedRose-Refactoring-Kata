8cab6ec9b79cf7510f81bc79f447cbb4f8137a60
NinaNekonoran <franzese.rozen@gmail.com>
2024-03-08 09:22:18 +0100

    fix: GildedRoseTest.foo()

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,12 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    /*@Test
+    @Test
     void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }*/
+        assertEquals("foo", ((Item)app.getItems().get(0)).name);
+    }
 
 }
